f16de3173f07cb191f1aa1add8d8ce5eba22d61f
des-yogi <nuevo-design@mail.ru>
2020-02-06 19:51:19 +0200

    footer in progress

---
+++ src/blocks/event-slider/event-slider.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var eventSwiper = new Swiper('.event-slider__container', {
+      slidesPerView: 1,
+      spaceBetween: 24,
+      loop: true,
+      pagination: {
+        el: '.event-slider__pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.event-slider__next',
+        prevEl: '.event-slider__prev',
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+        },
+        1440: {
+          slidesPerView: 3,
+        },
+      }
+  });
+});
